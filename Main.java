f1bd2da77988c41a52e10c8354aa37833f8581ec
Dulce Granados <dulcegranados.chun06@gmail.com>
2025-07-31 07:40:53 -0600

    nuevo main

--- Main.java
+++ Main.java
@@ -1,24 +1,17 @@
 import java.util.Scanner;
-
-import biblioteca1.model.Lector;
 public class Main {
 public static void main(String[] args) {
 
     Lector lector;
 
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Ingrese el sexo del lector: ");
-    String sexo = scanner.nextLine();
-    System.out.print("Ingrese la nacionalidad del lector: ");
-    String nacionalidad = scanner.nextLine();
-    lector = new Lector(sexo, nacionalidad); 
-    System.out.println("Nuevo lector ingresado. Datos: sexo: " + sexo + "Nacionalidad: " + nacionalidad);
-
+  
+    BibliotecaController bibliotecaController = new BibliotecaController();
 
     while (true) {
-    System.out.println("1. Solicitar un libro");
-    System.out.println("2. Estudiar en la biblioteca");
-    System.out.println("3. Investigar una noticia");
+    System.out.println("1. Registrar Lector");
+    System.out.println("2. Registrar bibliotecaria");
+    System.out.println("3. Pedir Enciclopedia");
     System.out.println("4. Preguntar algo a la bibliotecaria");
     System.out.println("5. Salir de la biblioteca");
 
@@ -27,11 +20,23 @@ public static void main(String[] args) {
     scanner.nextLine();
 
         switch (opcion) {
+
+    case 0: 
+    System.out.print("Ingrese el sexo del lector: ");
+    String sexo = scanner.nextLine();
+    System.out.print("Ingrese la nacionalidad del lector: ");
+    String nacionalidad = scanner.nextLine();
+    bibliotecaController.crearLector(sexo, nacionalidad);
+
     case 1:
-    System.out.print("Ingrese el nombre del libro que desea: ");
-    String libroDeseado = scanner.nextLine();
+    System.out.print("Ingrese el nombre de la bibliotecaria: ");
+    String nombre = scanner.nextLine();
+    System.out.print("Ingrese el turno de la bibliotecaria: ");
+    String turno = scanner.nextLine();
+    System.out.print("Ingrese la edad de la bibliotecaria: ");
+    String edad = scanner.nextLine();
     scanner.nextLine();
-    lector.pideLibro(libro);
+    bibliotecaController.crearBibliotecaria(nombre, turno, edad);
     break;
     case 2:
     lector.estudiar();
